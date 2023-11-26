f49c0059aca562294c8967d906531729e62cc4e6
John Duncan <john.a.duncan@utexas.edu>
2023-11-25 19:44:18 -0600

    Detector converter works

--- launch/compute_nuscenes_results.launch.py
+++ launch/compute_nuscenes_results.launch.py
@@ -2,6 +2,7 @@ import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import ExecuteProcess, IncludeLaunchDescription
+from launch_ros.actions import Node
 from launch_xml.launch_description_sources import XMLLaunchDescriptionSource
 # TODO - programmatically find bag path
 
@@ -14,11 +15,20 @@ def generate_launch_description():
             "bag",
             "play",
             "--loop",
-            "/home/jd/tracking_ws/src/ros_tracking/data/detections/mcap/NuScenes-v1.0-mini-scene-0061-megvii/NuScenes-v1.0-mini-scene-0061-megvii_0.mcap",
+            "/home/jd/nuscenes2mcap/output/NuScenes-v1.0-mini-scene-0061-megvii/NuScenes-v1.0-mini-scene-0061-megvii_0.mcap",
         ],
         output="screen",
     )
     ld.add_action(bag)
+
+    # Detection conversion node
+    det_node = Node(
+        package='ros_tracking',
+        executable='det_converter',
+        name='nuscenes_converter',
+        remappings=[('/nuscenes_detections','/detections' )]
+    )
+    ld.add_action(det_node)
 
     # Foxglove bridge for visualization
     bridge = IncludeLaunchDescription(
