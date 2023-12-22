202ef6ce81f996eeeb3e894b61c9c5fa69f38d84
John Duncan <john.a.duncan@utexas.edu>
2023-12-21 18:25:57 -0600

    Add oak-d launch files and update converter

--- launch/oakd_demo_launch.py
+++ launch/oakd_demo_launch.py
@@ -11,41 +11,58 @@ from launch import LaunchDescription
 from launch_ros.actions import Node
 
 def generate_launch_description():
+    ld = LaunchDescription()
 
-    return LaunchDescription([
+    # Config file
+    config = os.path.join(
+        get_package_share_directory('ros_tracking'),
+        'config',
+        'oakd.yaml'
+    )
 
-        # Sensor node
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource([
-                PathJoinSubstitution([
-                    FindPackageShare('depthai_ros_driver'),
-                    'launch',
-                    'camera.launch.py'
-                ])
-            ]),
-            launch_arguments={
-                'params_file': PathJoinSubstitution([
-                    FindPackageShare('ros_tracking'),
-                    'config',
-                    'oakd.yaml'
-                ])
-            }.items()
-        ),
+    # Static TF node
+    tf_node = Node(package = "tf2_ros", 
+                    executable = "static_transform_publisher",
+                    arguments = ["0", "0", "1.0", "0", "0", "0", "map", "oak-d-base-frame"]
+    )
+    ld.add_action(tf_node)
 
-        # Detection conversion node
-        Node(
-            package='ros_tracking',
-            executable='depthai_converter',
-            name='depthai_converter',
-            remappings=[('/depthai_detections','/oak/nn/spatial_detections')]
-        ),
+    # Detection conversion node
+    conv_node = Node(
+        package='ros_tracking',
+        executable='depthai_converter',
+        name='depthai_converter_node',
+        remappings=[('/depthai_detections','/oak/nn/spatial_detections')],
+        output='screen',
+        parameters=[config])    
+    ld.add_action(conv_node)
 
-        # Foxglove bridge for visualization
-        IncludeLaunchDescription(
-            XMLLaunchDescriptionSource(
-                os.path.join(
-                    get_package_share_directory('foxglove_bridge'),
-                    'launch/foxglove_bridge_launch.xml'))
-        )
+    # Sensor node
+    cam_node = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([
+            PathJoinSubstitution([
+                FindPackageShare('depthai_ros_driver'),
+                'launch',
+                'camera.launch.py'
+            ])
+        ]),
+        launch_arguments={
+            'params_file': PathJoinSubstitution([
+                FindPackageShare('ros_tracking'),
+                'config',
+                'oakd.yaml'
+            ])
+        }.items()
+    )
+    ld.add_action(cam_node)
 
-    ])
+    # Foxglove bridge for visualization
+    viz_node = IncludeLaunchDescription(
+        XMLLaunchDescriptionSource(
+            os.path.join(
+                get_package_share_directory('foxglove_bridge'),
+                'launch/foxglove_bridge_launch.xml'))
+    )
+    ld.add_action(viz_node)
+
+    return ld
