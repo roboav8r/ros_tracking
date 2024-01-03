9fe528fefa563e6b30ee5a85b99333a19a39cdb5
John Duncan <john.a.duncan@utexas.edu>
2024-01-03 11:04:37 -0600

    OAKD update

--- src/depthai_converter.cpp
+++ src/depthai_converter.cpp
@@ -2,11 +2,15 @@
 
 #include "rclcpp/rclcpp.hpp"
 
+#include "tf2_ros/transform_listener.h"
+#include "tf2_ros/buffer.h"
+#include "tf2_geometry_msgs/tf2_geometry_msgs.hpp"
+
 #include "vision_msgs/msg/detection3_d_array.hpp"
-#include "geometry_msgs/msg/vector3.hpp"
-#include "foxglove_msgs/msg/scene_update.hpp"
-#include "foxglove_msgs/msg/scene_entity.hpp"
-#include "foxglove_msgs/msg/cube_primitive.hpp"
+#include "tracking_msgs/msg/detections3_d.hpp"
+#include "tracking_msgs/msg/detection3_d.hpp"
+#include "geometry_msgs/msg/pose_stamped.hpp"
+
 
 using std::placeholders::_1;
 
@@ -17,20 +21,24 @@ class DetConverter : public rclcpp::Node
 {
   public:
     DetConverter()
-    : Node("detection_converter")
+    : Node("depthai_converter_node")
     {
-
       this->subscription_ = this->create_subscription<vision_msgs::msg::Detection3DArray>(
       "depthai_detections", 10, std::bind(&DetConverter::topic_callback, this, _1));
+      this->publisher_ = this->create_publisher<tracking_msgs::msg::Detections3D>("converted_detections", 10);
 
-      this->scene_publisher_ = this->create_publisher<foxglove_msgs::msg::SceneUpdate>("foxglove_detections", 10);
-      // TODO - converted detections
+      this->declare_parameter("tracker_frame",rclcpp::ParameterType::PARAMETER_STRING);
+      this->declare_parameter("labels",rclcpp::ParameterType::PARAMETER_STRING_ARRAY);
+      
+      this->tracker_frame_ = this->get_parameter("tracker_frame").as_string();
+      this->labels_ = this->get_parameter("labels").as_string_array();
 
-      this->scene_msg_ = foxglove_msgs::msg::SceneUpdate();
-      this->entity_msg_ = foxglove_msgs::msg::SceneEntity();
-      this->cube_ = foxglove_msgs::msg::CubePrimitive();
-      this->scene_msg_.entities.reserve(this->max_dets_);
-      // TODO - add detection messages, reserve space
+      this->tf_buffer_ =std::make_unique<tf2_ros::Buffer>(this->get_clock());
+      this->tf_listener_ = std::make_shared<tf2_ros::TransformListener>(*tf_buffer_);
+
+      this->det_msg_ = tracking_msgs::msg::Detection3D();
+      this->dets_msg_ = tracking_msgs::msg::Detections3D();
+      this->dets_msg_.detections.reserve(this->max_dets_);
     }
 
   private:
@@ -39,51 +47,33 @@ class DetConverter : public rclcpp::Node
      
       if (msg->detections.size() > 0) // If there are messages to be published
       {
-        // TODO - initialize dets_msg_
-        this->scene_msg_ = foxglove_msgs::msg::SceneUpdate();
+        this->dets_msg_ = tracking_msgs::msg::Detections3D();
+        this->dets_msg_.header.stamp = msg->header.stamp;     
+        this->dets_msg_.header.frame_id = this->tracker_frame_;
 
         for (auto it = msg->detections.begin(); it != msg->detections.end(); it++)
         {
               int n = it - msg->detections.begin();
-              this->entity_msg_ = foxglove_msgs::msg::SceneEntity();
+              this->det_msg_ = tracking_msgs::msg::Detection3D();
 
-              // Add header data
-              this->entity_msg_.timestamp = msg->header.stamp;
-              this->entity_msg_.frame_id = msg->header.frame_id;
-              this->entity_msg_.id = std::to_string(n);
-
-              // Add bounding box data
-              this->cube_ = foxglove_msgs::msg::CubePrimitive();
-              cube_.pose = it->results[0].pose.pose;
-              cube_.size = geometry_msgs::msg::Vector3();
-              cube_.size.x = 0.5;
-              cube_.size.y = 0.5;
-              cube_.size.z = 0.5;
+              // Convert spatial information
+              this->obj_pose_det_frame_ = geometry_msgs::msg::PoseStamped();
+              this->obj_pose_det_frame_.header = msg->header;
+              this->obj_pose_det_frame_.pose = it->results[0].pose.pose;
+              this->obj_pose_trk_frame_ = this->tf_buffer_->transform(this->obj_pose_det_frame_,this->tracker_frame_);
+              this->det_msg_.pose = obj_pose_trk_frame_.pose;
+              this->det_msg_.bbox.center = obj_pose_trk_frame_.pose;
+              this->det_msg_.bbox.size = it->bbox.size;
 
               // Add semantic information
+              this->det_msg_.class_string = this->labels_[std::stoi(it->results[0].hypothesis.class_id)];
+              this->det_msg_.class_confidence = it->results[0].hypothesis.score;
 
-              // Add to entity
-              this->entity_msg_.cubes.emplace_back(cube_);
-
-              // Add entity to scene update
-              this->scene_msg_.entities.emplace_back(this->entity_msg_);
-
-              //this->det_msg_ = tracking_msgs::msg::Detection3D();
-              
-              // // Convert entity to detection3d
-              // this->det_msg_.pose = it->cubes[0].pose;
-              // this->det_msg_.class_string = it->metadata[0].value;
-              // this->det_msg_.class_confidence = std::stof(it->metadata[1].value);
-              // this->det_msg_.attribute = it->metadata[2].value;
-              // this->det_msg_.bbox.center = it->cubes[0].pose;
-              // this->det_msg_.bbox.size = it->cubes[0].size;
-
-              // // Add detection to Detections3d
-              // this->dets_msg_.detections.emplace_back(det_msg_);
+              this->dets_msg_.detections.emplace_back(this->det_msg_);
         }
 
-        // this->publisher_->publish(dets_msg_);
-        this->scene_publisher_->publish(scene_msg_);
+        this->publisher_->publish(this->dets_msg_);
+        // this->scene_publisher_->publish(scene_msg_);
 
       } else { // Don't publish a message
         return;
@@ -91,13 +81,20 @@ class DetConverter : public rclcpp::Node
       }
            
     }
-    
+
+    std::string tracker_frame_;
+    std::shared_ptr<tf2_ros::TransformListener> tf_listener_{nullptr};
+    std::unique_ptr<tf2_ros::Buffer> tf_buffer_;
+    geometry_msgs::msg::PoseStamped obj_pose_det_frame_;
+    geometry_msgs::msg::PoseStamped obj_pose_trk_frame_;
+
     rclcpp::Subscription<vision_msgs::msg::Detection3DArray>::SharedPtr subscription_;
-    rclcpp::Publisher<foxglove_msgs::msg::SceneUpdate>::SharedPtr scene_publisher_;
-    foxglove_msgs::msg::SceneUpdate scene_msg_;
-    foxglove_msgs::msg::SceneEntity entity_msg_;
-    foxglove_msgs::msg::CubePrimitive cube_;
+    rclcpp::Publisher<tracking_msgs::msg::Detections3D>::SharedPtr publisher_;
+    tracking_msgs::msg::Detections3D dets_msg_;
+    tracking_msgs::msg::Detection3D det_msg_;
     int max_dets_{250}; 
+
+    std::vector<std::string> labels_;
 };
 
 int main(int argc, char * argv[])
